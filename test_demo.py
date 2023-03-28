1c0c5652dbd063817c9ec550edab841c56294030
mehmetcannn0 <mehmetceraslan@gmail.com>
2023-03-28 13:53:55 +0300

    6. ders eklemelerı

--- test_demo.py
+++ test_demo.py
@@ -8,36 +8,38 @@ from selenium.webdriver.common.action_chains import ActionChains
 import pytest
 from pathlib import Path
 from datetime import date
-#prefix => ön ek test_
-#postfix
+import openpyxl
+from constants import globalConstants
+
 
 class Test_DemoClass:
     #her testten önce çağırılır
     def setup_method(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window()
-        self.driver.get("https://www.saucedemo.com/")
+        self.driver.get(globalConstants.URL)
         self.folderPath = str(date.today())
         Path(self.folderPath).mkdir(exist_ok=True)
-        #24.03.2023
+       
         # günün tarihini al bu tarih ile bir klasör var mı kontrol et yoksa oluştur
         
     #her testten sonra çağırılır
     def teardown_method(self):
         self.driver.quit()
 
-    def readData(self):
-        print("x")
-    # setup -> test_demoFunc -> teardown
-    def test_demoFunc(self):
-        # 3A Act Arrange Assert
-        text = "Hello"
-        assert text == "Hello"
-    # setup -> test_demo2 -> teardown
-    def test_demo2(self):
-        assert True
-
-    @pytest.mark.parametrize("username,password",[("1","1") , ("kullaniciadim","sifrem")])
+    def getData():
+        #veriyi al
+        excelFile = openpyxl.load_workbook("data/invalid_login.xlsx")
+        selectedSheet = excelFile["Sheet1"]
+        totalRows = selectedSheet.max_row
+        data=[]
+        for i in range(2, totalRows+1):
+            username = selectedSheet.cell(i,1).value
+            password = selectedSheet.cell(i,2).value
+            tupleData = (username,password)
+            data.append(tupleData)
+        return data
+    @pytest.mark.parametrize("username,password",getData())
     def test_invalid_login(self,username,password):
         self.waitForElementVisible((By.ID,"user-name"))
         usernameInput = self.driver.find_element(By.ID, "user-name")
