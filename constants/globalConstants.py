14de39f99d68ea404ffbd73ae33425e9d77409d5
mehmetcannn0 <mehmetceraslan@gmail.com>
2023-03-29 17:52:42 +0300

    global constants eklendı

--- constants/globalConstants.py
+++ constants/globalConstants.py
@@ -1,1 +1,6 @@
 URL="https://www.saucedemo.com/"
+name_of_excel="data\data.xlsx"
+invalid_login_error_message_text ="Epic sadface: Username and password do not match any user in this service"
+user_name_req_errorMessage_text = "Epic sadface: Username is required"
+pass_req_errorMessage_text = "Epic sadface: Password is required"
+locked_out_user_errorMessage_text = "Epic sadface: Sorry, this user has been locked out."
